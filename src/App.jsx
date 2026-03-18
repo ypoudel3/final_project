41bb07e1c89418cf005018acd7d2bd68dd6e60e4
amisadhakal <amisadhakal0@gmail.com>
2026-03-18 18:45:03 +0545

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,10 @@
 import LandingPage from "./components/Landingpage.jsx";
-import Features from "./components/Features.jsx";
-import Process from "./components/Process.jsx";
-import Choose from "./components/Choose.jsx";
-import Testimonial from "./components/Testimonial.jsx";
-import Connect from "./components/Connect.jsx";
+
 function App() {
   return (
     <>
       <LandingPage />
-      <Features />
-      <Process />
-      <Choose />
-      <Testimonial />
-      <Connect />
+      
     </>
   );
 }
